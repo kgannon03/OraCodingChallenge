84829fe00ec5abd2bac95b67559c79bc92fcf0e0
Kevin Gannon <kgannon@solstice-consulting.com>
2017-02-03 13:57:22 -0600

    Account ViewModel

--- OraCodeChallenge/AccountViewModel.swift
+++ OraCodeChallenge/AccountViewModel.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class AccountViewModel {
+    
+    let service: AccountService
+    let disposeBag = DisposeBag()
+    let userResponse: Observable<APIResponse<UserResponse>>
+    
+    var loadingState = Variable<LoadingState>(.Loading)
+    var userEmail = Variable<String>("")
+    var userName = Variable<String>("")
+    
+    init(service: AccountService) {
+        self.service = service
+        userResponse = service.getUser()
+        
+        addBindings()
+    }
+    
+    func addBindings() {
+        userResponse
+            .map{ $0.data?.user.name ?? ""}
+            .bindTo(userName)
+            .addDisposableTo(disposeBag)
+        
+        userResponse
+            .map{ $0.data?.user.email ?? ""}
+            .bindTo(userEmail)
+            .addDisposableTo(disposeBag)
+        
+        userResponse
+            .asObservable()
+            .map{ next in
+                guard let _ = next.data else { return .Error }
+                return .Loaded
+            }
+            .bindTo(loadingState)
+            .addDisposableTo(disposeBag)
+    }
+}
