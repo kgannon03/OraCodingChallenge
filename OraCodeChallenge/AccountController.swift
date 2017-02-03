c56f2898c6392e812313fabeff136d7620047ffa
Kevin Gannon <kgannon@solstice-consulting.com>
2017-02-03 14:04:40 -0600

    Account controller bindings

--- OraCodeChallenge/AccountController.swift
+++ OraCodeChallenge/AccountController.swift
@@ -7,21 +7,40 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class AccountController: UIViewController {
 
     @IBOutlet weak var email: OraFormField!
-    @IBOutlet weak var password: OraFormField!
+    @IBOutlet weak var name: OraFormField!
     @IBOutlet weak var save: OraButton!
     @IBOutlet weak var loaderView: UIView!
+    
+    let viewModel = AccountViewModel(service: AccountService())
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        addBindings()
     }
-
     
-
-
-
+    func addBindings() {
+        viewModel.loadingState
+            .asObservable()
+            .map{ $0 == .Loading ? false : true }
+            .bindTo(loaderView.rx.isHidden)
+            .addDisposableTo(disposeBag)
+        
+        viewModel.userEmail
+            .asObservable()
+            .bindTo(email.rx.text)
+            .addDisposableTo(disposeBag)
+        
+        viewModel.userName
+            .asObservable()
+            .bindTo(name.rx.text)
+            .addDisposableTo(disposeBag)
+    }
 }
